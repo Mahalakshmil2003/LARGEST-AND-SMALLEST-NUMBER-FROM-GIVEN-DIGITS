080362ab514cb3b58faaaa8024696ef6ddec1ca9
Mahalakshmil2003 <95754399+Mahalakshmil2003@users.noreply.github.com>
2022-07-22 18:49:24 +0530

    Update DigitNumber.java

--- DigitNumber.java
+++ DigitNumber.java
@@ -6,9 +6,7 @@ class DigitNumber{
 		int temp = 0;  
       		for(int i=0; i < n; i++){  
                  	for(int j=1; j < (n-i); j++){  
-                          if(a[j-1] > a[j]){  
                                     swap(a,j);
-                      	}
                       }  
                 }  
            }
@@ -16,11 +14,12 @@ class DigitNumber{
 	//swap
 	static void swap (int arr[],int j)
 	{
-		arr[j-1]=arr[j-1]+arr[j];
-		arr[j]=arr[j-1]-arr[j];
-		arr[j-1]=arr[j-1]-arr[j];
+		if(arr[j-1] > arr[j]){
+			arr[j-1]=arr[j-1]+arr[j];
+			arr[j]=arr[j-1]-arr[j];
+			arr[j-1]=arr[j-1]-arr[j];
+		}
 	}
-	
 	//read digits
 	static void readDigits(int a[],int num){
 		Scanner sc=new Scanner(System.in);
